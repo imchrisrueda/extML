e8a4bb0179be39f3140bc1fadfe3b2f8f6cac16a
aidaggg <aidagarciaech@gmail.com>
2026-02-19 18:22:43 +0100

    cambio en epsilon = 0

--- k_brazos/src/algorithms/epsilon_greedy.py
+++ k_brazos/src/algorithms/epsilon_greedy.py
@@ -30,6 +30,7 @@ class EpsilonGreedy(Algorithm):
 
         super().__init__(k)
         self.epsilon = epsilon
+        self.selected_arms = set()  # Conjunto para llevar el registro de brazos seleccionados
 
     def select_arm(self) -> int:
         """
@@ -40,16 +41,30 @@ class EpsilonGreedy(Algorithm):
 
         # Observa que para para epsilon=0 solo selecciona un brazo y no hace un primer recorrido por todos ellos.
         # ¿Podrías modificar el código para que funcione correctamente para epsilon=0?
+        # modificar el código proque para epsilon = 0 devuelve siempre el brazo 0, solo explora ese brazo
 
-        if np.random.random() < self.epsilon:
-            # Selecciona un brazo al azar
-            chosen_arm = np.random.choice(self.k)
+        #para cuando epsilon = 0 se va a hacer : probar al menos una vez todos los brazo para después empezar a explotar el mejor.
+
+        # Asegurar que cada brazo se pruebe al menos una vez
+        # self.k es el número total de brazos disponibles
+        # y self.selected_arms almacena los índices de los brazos que ya han sido seleccionados
+        # por lo que si el conjunto de índices seleccionas es menor que el número total de brazos disponibles, significa que no se han seleccionado todos los brazos
+        if len(self.selected_arms) < self.k:
+            # Selecciona un brazo no probado
+            chosen_arm = len(self.selected_arms)  # Escoge el próximo brazo no probado
+            self.selected_arms.add(chosen_arm)  # Agrega el brazo al conjunto de seleccionados
+
+        # si todos los brazos ya han sido seleccionados al menos una vez:
+        # igual que antes
         else:
-            # Selecciona el brazo con la recompensa promedio estimada más alta
-            chosen_arm = np.argmax(self.values)
+            if np.random.random() < self.epsilon:
+                # Selecciona un brazo al azar
+                chosen_arm = np.random.choice(self.k)
+            else:
+                # Selecciona el brazo con la recompensa promedio estimada más alta
+                chosen_arm = np.argmax(self.values)
 
         return chosen_arm
-
 
 
 
