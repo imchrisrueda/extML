f1897f31f2ca7c28f86f41c02c887684f5d66916
aidaggg <aidagarciaech@gmail.com>
2026-03-05 22:06:37 +0100

    arreglado mc

--- entornos_complejos/src/agent.py
+++ entornos_complejos/src/agent.py
@@ -41,31 +41,20 @@ class AgentMC:
 
     def get_action(self, state) -> Any:
         """
-        Qué acción realizar según el estado.
+        Indicará qué acción realizar de acuerdo al estado.
         Responde a la política del agente --> política epsilon-greedy basada en Q
         """
-        probs = np.ones(self.nA) * self.epsilon / self.nA # al principio todas las acciones tienen la misma probabilidad
-        best_action = np.argmax(self.Q[state])  # la mejor acción es la de más Q
-        probs[best_action] += (1.0 - self.epsilon) # ahora lay mayor prob va a tener la de maor Q
+        probs = np.ones(self.nA) * self.epsilon / self.nA # Al principio todas las acciones tienen la misma probabilidad
+        best_action = np.argmax(self.Q[state])  # La mejor acción es la de más Q
+        probs[best_action] += (1.0 - self.epsilon) # Ahora la mayor prob va a tener la de mayor Q
 
-        return np.random.choice(np.arange(self.nA), p=probs) # en base a las probabilidades elegir una acción
+        return np.random.choice(np.arange(self.nA), p=probs) # En base a las probabilidades elegir una acción
 
 
-
-    # para off policy
-    def behavior_action(self, state):
-        """
-        Política de comportamiento b(a|s)
-        Totalmente aleatoria 
-        """
-        return self.env.action_space.sample()
-    
-
-    # este es el update all visits del apartado A
     def update(self, obs, action, next_obs, reward, terminated, truncated, info):
         """
-        Con (s, a, s', r) aplicar MC all-visit
-        estado, accion, siguiente estado, recompensa
+        Con la muestra (s, a, s', r) e información complementaria aplicamos el algoritmo.
+        Implementación Monte Carlo all-sisit
         """
 
         # Guardar combinación - s,a,r
@@ -76,58 +65,38 @@ class AgentMC:
 
         done = terminated or truncated
 
-        # si el episodio ha terminado se actualiza MC
+        # Si el episodio ha terminado se actualiza MC
         if done:
             G = 0
 
             for t in reversed(range(len(self.episode))): # reversed porque es hacia atrás
-                s, a, r = self.episode[t] # sacar valores del episodio
-                G = r + self.gamma * G # retorno
+                s, a, r = self.episode[t] # Sacar valores del episodio
+                G = self.gamma * G + r # Retorno
 
                 self.returns_count[s, a] += 1 
                 alpha = 1.0 / self.returns_count[s, a] # promedio 
 
                 self.Q[s, a] += alpha * (G - self.Q[s, a])
 
-            # reset episodio
+            # Reset episodio
             self.episode = []
             self.episode_return = 0.0
             self.episode_length = 0
 
+    
 
-    # update de forma on-policy Q[s,a] += (1/N) * (G - Q[s,a])
-    # first visit!!
-    def update_on(self, obs, action, next_obs, reward, terminated, truncated, info):
+    # para off policy
+    def behavior_action(self, state):
         """
-        Con (s, a, s', r) aplicar MC all-visit
-        estado, accion, siguiente estado, recompensa
+        Política de comportamiento b(a|s)
+        Totalmente aleatoria 
         """
-        self.episode.append((obs, action, reward))
-        self.episode_return += reward
-        self.episode_length += 1
-
-        done = terminated or truncated
-
-        if done:
-            G = 0
-            visited = set()  
-
-            for t in reversed(range(len(self.episode))):
-                s, a, r = self.episode[t]
-                G = r + self.gamma * G
-
-                if (s, a) not in visited:
-                    visited.add((s, a))
-                    self.returns_count[s, a] += 1
-                    alpha = 1.0 / self.returns_count[s, a]
-                    self.Q[s, a] += alpha * (G - self.Q[s, a])
-
-            self.episode = []
-            self.episode_return = 0.0
-            self.episode_length = 0
-
-
-    # este caso de off policy 
+        probs = np.ones(self.nA) * self.epsilon / self.nA
+        best_action = np.argmax(self.Q[state])
+        probs[best_action] += (1.0 - self.epsilon)
+        return np.random.choice(np.arange(self.nA), p=probs)
+    
+     # este caso de off policy 
     def update_off(self, obs, action, next_obs, reward, terminated, truncated, info):
         self.episode.append((obs, action, reward))
         self.episode_return += reward
@@ -157,7 +126,7 @@ class AgentMC:
                     break  # si π(a|s) = 0, terminamos
                 
                 # Política de comportamiento b: aleatoria uniforme
-                prob_b = 1.0 / self.nA
+                prob_b = self.epsilon / self.nA + (1.0 - self.epsilon) 
                 
                 # Actualizar peso de importancia
                 W *= 1.0 / prob_b  # π(a|s) = 1, así que solo dividimos por prob_b
@@ -171,7 +140,9 @@ class AgentMC:
             self.episode_length = 0
 
     # disminuir epsilon para que al final vaya explorando menos y explotando más
-    # porque ahí ya sabemos cuál es la mejor opción, ha ido aprendiendo
+    # porque ahí ya sabemos cuál es la mejor opción
     def decay_epsilon(self, episode_number):
         if self.decay:
-            self.epsilon = max(0.01, 1.0 - episode_number * (0.99 / 8000))
+            self.epsilon = min(1.0, 1000.0 / (episode_number + 1))
+            # self.epsilon = max(0.01, 1.0 - episode_number * (0.99 / 8000))
+
