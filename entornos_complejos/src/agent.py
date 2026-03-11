ce96e84a537b55ba67f9d2382df8105c2499d9df
imchrisrueda <chsrueda@gmail.com>
2026-03-11 13:58:08 +0100

    Add Aida's MC

--- entornos_complejos/src/agent.py
+++ entornos_complejos/src/agent.py
@@ -22,24 +22,19 @@ class AgentMC:
         """
 
         self.env = env
-        self.nA = env.action_space.n # Número de acciones
-        self.nS = env.observation_space.n # Número de estados
+        self.nA = env.action_space.n # num de acciones
+        self.nS = env.observation_space.n # num de estados
 
-        # Inicializar todo
         self.epsilon = epsilon
         self.gamma = gamma
         self.decay = decay
 
         # Q(s,a) - valor esperado 
         self.Q = np.zeros((self.nS, self.nA)) 
+        self.returns_count = np.zeros((self.nS, self.nA)) # contador de visitas
+        self.C = np.zeros((self.nS, self.nA)) # suma de pesos     
 
-        # Contador de visitas
-        self.returns_count = np.zeros((self.nS, self.nA)) 
-
-        # Memoria del episodio
         self.episode = []
-
-        # Estadísticas
         self.episode_return = 0.0
         self.episode_length = 0
 
@@ -88,9 +83,64 @@ class AgentMC:
             self.episode_return = 0.0
             self.episode_length = 0
 
+    
+
+    # para off policy
+    def behavior_action(self, state):
+        """
+        Política de comportamiento b(a|s)
+        Totalmente aleatoria 
+        """
+        probs = np.ones(self.nA) * self.epsilon / self.nA
+        best_action = np.argmax(self.Q[state])
+        probs[best_action] += (1.0 - self.epsilon)
+        return np.random.choice(np.arange(self.nA), p=probs)
+    
+     # este caso de off policy 
+    def update_off(self, obs, action, next_obs, reward, terminated, truncated, info):
+        self.episode.append((obs, action, reward))
+        self.episode_return += reward
+        self.episode_length += 1
+
+        done = terminated or truncated
+
+        if done:
+            G = 0.0 # retorno acumulado 
+            W = 1.0  # ratio de importancia acumulado
+
+            for t in reversed(range(len(self.episode))): # recorrer hacia atrás
+                s, a, r = self.episode[t]
+                G = r + self.gamma * G
+
+                # Actualización con Weighted Importance Sampling
+                self.C[s, a] += W
+                self.returns_count[s, a] += 1
+                self.Q[s, a] += (W / self.C[s, a]) * (G - self.Q[s, a])
+
+                # Calcular ratio de importancia para el paso anterior
+                # Política objetivo π: greedy respecto a Q
+                greedy_action = np.argmax(self.Q[s])
+                
+                # π(a|s) = 1 si a es la acción greedy, 0 si no
+                if a != greedy_action:
+                    break  # si π(a|s) = 0, terminamos
+                
+                # Política de comportamiento b: aleatoria uniforme
+                prob_b = self.epsilon / self.nA + (1.0 - self.epsilon) 
+                
+                # Actualizar peso de importancia
+                W *= 1.0 / prob_b  # π(a|s) = 1, así que solo dividimos por prob_b
+
+                if W > 1e6:  # límite de estabilidad
+                    break
+
+            self.episode = []
+            self.episode_return = 0.0
+            self.episode_length = 0
+
     # disminuir epsilon para que al final vaya explorando menos y explotando más
     # porque ahí ya sabemos cuál es la mejor opción
     def decay_epsilon(self, episode_number):
         if self.decay:
             self.epsilon = min(1.0, 1000.0 / (episode_number + 1))
-
+            # self.epsilon = max(0.01, 1.0 - episode_number * (0.99 / 8000))
